0653345cd347e309619993739c4ec425345c8eba
GlobalOffender <99067159@mydavinci.nl>
2021-09-09 12:58:02 -0800

    Kaas

--- program.py
+++ program.py
@@ -1,6 +1,29 @@
-a = input('getal ')
-b = input('getal2 ')
-if a > b:
-    print('a^')
+geel = input('Is de kaas geel? [Y/N]: ').lower()
+if geel == "y":
+    gaten = input('Zitten er gaten in? [Y/N]: ').lower()
+    if gaten == "y":
+        prijs = input('Is de kaas belachelijk duur? [Y/N]: ').lower()
+        if prijs == "y":
+            print('Emmenthaler')
+        else:
+            print('Leerdammer')
+    else:
+        steen = input('Is de kaas hard als steen? [Y/N]: ').lower()
+        if steen == "y":
+            print('Panmigiano Reggiano')
+        else:
+            print('Goudse kaas')
 else:
-    print('b^')
+    schimmel = input('Heeft de kaas blauwe schimmels? [Y/N]: ').lower()
+    if schimmel == "y":
+        korst1 = input('heeft de kaas een korst? [Y/N]: ').lower()
+        if korst1 == "y":
+            print('Bleu de Rochbaron')
+        else:
+            print("Foume d'Ambert")
+    else:
+        korst2 = input('heeft de kaas een korst? [Y/N]: ').lower()
+        if korst2 == "y":
+            print("Camembert")
+        else:
+            print("Mozzarella")    
